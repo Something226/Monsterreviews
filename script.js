d4367fafd4c96093b882985fdcf345b3b42bd436
Something226 <axel.lieback@gmail.com>
2021-05-30 23:29:22 +0200

    Final commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function deleteElement() {
+    var deleteObj = document.getElementById("test");
+    deleteObj.innerHTML = '';
+  }
+
+function addElement() {
+   var element = document.getElementById("test");
+   element.innerHTML = '<p>I know some javascript</p>';
+}
